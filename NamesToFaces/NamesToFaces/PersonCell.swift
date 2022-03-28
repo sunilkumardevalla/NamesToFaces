3d8419502b9eba83aa539b35c2cb47ceba8fd22c
SunilKumarDevalla <sunilkumardevalla@gmail.com>
2022-03-28 10:38:15 -0500

    Add cell items override function.

---
+++ NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Sunny on 3/28/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
